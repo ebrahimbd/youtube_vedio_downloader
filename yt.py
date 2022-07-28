07836479836f75ee780c40a00959fced8f2a40fe
ebrahimbd <ebrahim16sust@gmail.com>
2022-07-28 17:18:33 +0600

    fix audio issu

--- yt.py
+++ yt.py
@@ -8,11 +8,21 @@ from pyfiglet import *
 # Here is some valid colors that we can use to color our art
 # valid_color = ('red', 'green', 'yellow', 'blue', 'cyan', 'white')
 
-ascii_art =figlet_format("Youtube free vedio Downloader ", font = "digital" )
+ascii_art =figlet_format("     Youtube    free    vedio    Downloader ", font = "digital" )
 colored_ascii = colored(ascii_art, 'red')
-ascii_art1 =figlet_format("© 2022 Developed by Ebrahim Mohammad Saleh https://salehcv.web.app/", font = "digital" )
-colored_ascii1 = colored(ascii_art1, 'cyan')
 print(colored_ascii)
+
+print(colored("All files are downloaded in the current directory--> %s"%os.getcwd(), 'yellow'))
+print("") 
+ 
+
+def copyright():
+    ascii_art1 =figlet_format("---- ---- Thanks For Download ---- ----  ", font = "digital" )
+    colored_ascii1 = colored(ascii_art1, 'cyan')
+    ascii_art2 =("© 2022 Developed by Ebrahim Mohammad Saleh , visit ->  https://salehcv.web.app/")
+    colored_ascii2= colored(ascii_art2, 'yellow')
+    print(colored_ascii1)
+    print(colored_ascii2)
 
 
 # fuchsia = '\033[38;2;255;00;255m'   #  color as hex #FF00FF
@@ -55,15 +65,16 @@ def single_vedio():
     global is_error_vedio
     try:
         video= YouTube(link)
-        video_get= YouTube(link).streams.filter(adaptive=True, file_extension='mp4').order_by('resolution').desc().first()
-        print("\nDownloading please wait .........") 
+        video_get= YouTube(link).streams.get_highest_resolution()
+        print("\nPlease Wait Download Will Start Shortly .........") 
+        # video_get= YouTube(link).streams.filter(adaptive=True, file_extension='mp4').order_by('resolution').desc().first()
         with alive_bar(bar='blocks', spinner='waves3') as bar: 
             print(f'\n' + 'Downloaded : ',video.title, '~ viewed', video.views, 'times.', )
             video_get.download()
             # video.streams.get_highest_resolution().download("/mnt/Ebrahim/tutorial/playlist_download")
             bar()
         print("\n Videos are downloaded.✅")
-        print(colored_ascii1)
+        copyright()
     except:
         is_error_vedio=True
 
@@ -74,18 +85,19 @@ def playlist_vedio():
     try:
         yt_playlist = Playlist(link)
         split_line = yt_playlist.title.split("/")
+        print("\nPlease Wait Download Will Start Shortly .........") 
         playlist_name = ' '.join(split_line )
         create_file(playlist_name, yt_playlist)
-        print("\nDownloading please wait .........") 
         for video in yt_playlist.videos:
             with alive_bar(bar='blocks', spinner='waves3') as bar: 
                     print(f'\n' + 'Downloaded : ',video.title, '~ viewed', video.views, 'times.', )
                     # video.streams.get_highest_resolution().download("/mnt/Ebrahim/tutorial/playlist_download")
-                    down=video.streams.filter(adaptive=True, file_extension='mp4').order_by('resolution').desc().first()
+                    # down=video.streams.filter(adaptive=True, file_extension='mp4').order_by('resolution').desc().first()
+                    down=video.streams.get_highest_resolution()
                     down.download(playlist_name)
                     bar()
         print("\nAll videos are downloaded.✅")
-        print(colored_ascii1)
+        copyright()
     except:
         is_error_vedio=True
 
@@ -98,7 +110,8 @@ while i<1000:
         if i==2:
             break
     if i==999:
-        print("\nFailed invalid link or Connection Error.............!!!!!!!!!")
+        
+        print(colored("\nFailed invalid link or Connection Error.............!!", 'red'))
     else:
         if is_single_vedio:
             single_vedio()
